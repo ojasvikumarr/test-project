3f103e23bb563e7021c8ae5c6ce5c08c1f1adfaf
Ojasvi Kumar <ojasvikumar007@gmail.com>
2024-07-14 12:10:38 +0530

    add headers for cors

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,6 +2,14 @@ import { sql } from '@vercel/postgres';
 import CryptoJS from 'crypto-js';
 
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
     if (req.method === 'POST') {
         try {
             const { name, code, bday, phone, email, company, password, confirmpassword } = req.body;
