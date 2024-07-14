a1efc79e7b5079698e97d351facacd7741b2c538
Ojasvi Kumar <ojasvikumar007@gmail.com>
2024-07-14 12:04:25 +0530

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { UserProvider } from "@/context/UserContext";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function App({ Component, pageProps }) {
   return <>
